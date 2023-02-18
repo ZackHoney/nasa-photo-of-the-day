df64e8b06b3739b37295a5aef0d2a66c4c1ecce6
Zachary Honey <zhoney466@gmail.com>
2023-02-18 13:43:40 -0800

    Completed MVP tasks

--- src/Components/NasaPhoto.js
+++ src/Components/NasaPhoto.js
@@ -1,13 +1,40 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const StyledPhoto = styled.div` 
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+
+   h3{
+    font-size: 2rem;
+    color: ${pr => pr.theme.secondaryColor};
+   }
+  
+  .date{
+    color: ${pr => pr.theme.primaryColor};
+   }
+
+  .explanation {
+    max-width: 50%;
+    color: ${pr => pr.theme.thirdColor};
+  }
+  
+  img {
+    max-width: 50%;
+    object-fit: cover;
+  }
+  
+`
 
 const NasaPhoto = (props) => {
     return (
-        <div className='nasa-photo-wrapper'>
+        <StyledPhoto className='nasa-photo-wrapper'>
             <h3>{props.photo.title}</h3>
-            <p>{props.photo.date}</p>
+            <p className='date'>{props.photo.date}</p>
             <img src ={props.photo.hdurl} />
-            <p>{props.photo.explanation}</p>
-        </div>
+            <p className='explanation'>{props.photo.explanation}</p>
+        </StyledPhoto>
     )
 }
 
