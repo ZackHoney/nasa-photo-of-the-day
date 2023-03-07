fef8f18acd749ced87cbcc0842300d11159d4c1b
Zachary Honey <zhoney466@gmail.com>
2023-03-07 08:41:19 -0700

    Edited the App.js file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from "react";
 import axios from 'axios';
-import NasaPhoto from './Components/NasaPhoto'
+import NasaPhoto from './Components/NasaPhoto';
 import "./App.css";
 
 
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import "./App.css";
 import theme from './Theme';
 
 import {ThemeProvider} from 'styled-components';
